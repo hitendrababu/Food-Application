994d65882a327c5f79f84c7643dfc0b011953278
hitendrababu <vnarla@uncc.edu>
2018-04-16 18:01:21 -0400

    RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,17 +10,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Samosa','Indian Samosa Recipe',
-    'https://inhabitat.com/wp-content/blogs.dir/1/files/2016/03/Leftovers-for-Hungry-Indian-Food.jpg'),
-    new Recipe('Chicken Masala','Indian Chicken Masala Recipe',
-    'https://thecitypaperbogota.com/wp-content/uploads/2015/11/curry-charles-haynes.jpg')
+  
+  recipes: Recipe[];
 
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,18 @@
+import { Recipe } from './recipe.model';
+
+
+export class RecipeService{
+
+    private recipes: Recipe[] = [
+        new Recipe('Samosa','Indian Samosa Recipe',
+        'https://inhabitat.com/wp-content/blogs.dir/1/files/2016/03/Leftovers-for-Hungry-Indian-Food.jpg'),
+        new Recipe('Chicken Masala','Indian Chicken Masala Recipe',
+        'https://thecitypaperbogota.com/wp-content/uploads/2015/11/curry-charles-haynes.jpg')
+    
+      ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+      
+}
