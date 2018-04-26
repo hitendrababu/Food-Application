bff8b688b9023baed8b7aaf9df47aee6a9c515df
hitendrababu <vnarla@uncc.edu>
2018-04-25 23:07:07 -0400

    Recipe___

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,37 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Injectable } from '@angular/core';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-
+@Injectable()
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Samosa','Indian Samosa Recipe',
-        'https://inhabitat.com/wp-content/blogs.dir/1/files/2016/03/Leftovers-for-Hungry-Indian-Food.jpg'),
-        new Recipe('Chicken Masala','Indian Chicken Masala Recipe',
-        'https://thecitypaperbogota.com/wp-content/uploads/2015/11/curry-charles-haynes.jpg')
-    
+        new Recipe('Samosa',
+        'Indian Samosa Recipe',
+        'https://inhabitat.com/wp-content/blogs.dir/1/files/2016/03/Leftovers-for-Hungry-Indian-Food.jpg',
+        [new Ingredient('Potato', 5),
+         new Ingredient('Onion',4)]),
+        new Recipe('Chicken Masala',
+        'Indian Chicken Masala Recipe',
+        'https://thecitypaperbogota.com/wp-content/uploads/2015/11/curry-charles-haynes.jpg',
+        [new Ingredient('Chicken', 5),
+        new Ingredient('Onion',4)])
       ];
+
+      constructor(private shoppingListService: ShoppingListService){
+
+      }
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
     }
       
 }
